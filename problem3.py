57dce0e912844414c488bffd35043a6e81728ae5
Dileep455 <124118695+Dileep455@users.noreply.github.com>
2023-02-01 10:00:47 +0530

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,8 @@
+k1,k2,k3=int(input("enter")),int(input("enter")),int(input("enter"))
+print(k1)
+print(k2)
+print(k3)
+j=k1+k2+k3
+print(j)
+k1,k2,k3=int(input("enter")),int(input("enter"))
+print(k1,k2,k3)
