b2e9103c38b2ee6abff3cde21deea17b19dbd660
Dileep455 <124118695+Dileep455@users.noreply.github.com>
2023-02-01 21:58:06 +0530

    Delete problem3.py

--- problem3.py
+++
@@ -1,8 +0,0 @@
-k1,k2,k3=int(input("enter")),int(input("enter")),int(input("enter"))
-print(k1)
-print(k2)
-print(k3)
-j=k1+k2+k3
-print(j)
-k1,k2,k3=int(input("enter")),int(input("enter"))
-print(k1,k2,k3)
